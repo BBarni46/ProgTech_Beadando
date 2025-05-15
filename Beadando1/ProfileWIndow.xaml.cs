9487a341a1e2dc1229a797e7eb4ee15d1cf21235
BBarni46 <barnabasbordak@gmail.com>
2025-05-15 20:20:44 +0200

    egyenlegFeltöltés

--- Beadando1/ProfileWIndow.xaml.cs
+++ Beadando1/ProfileWIndow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,10 +20,98 @@ namespace Beadando1
     /// </summary>
     public partial class ProfileWindow : Window
     {
+        private string username;
+        private string connectionString = "Server=localhost;Database=users;Uid=root;Pwd=;";
         public ProfileWindow(string username) 
         {
             InitializeComponent();
+            this.username = username;
             UsernameText.Text = $"Bejelentkezve: {username}";
+            LoadBalance();
+        }
+        private void LoadBalance()
+        {
+            try
+            {
+                using var connection = new MySqlConnection(connectionString);
+                connection.Open();
+
+                var command = new MySqlCommand("SELECT egyenleg FROM felhasználók WHERE név = @nev", connection);
+                command.Parameters.AddWithValue("@nev", username);
+
+                var result = command.ExecuteScalar();
+                if (result != null)
+                {
+                    double balance = Convert.ToDouble(result);
+                    BalanceText.Text = $"Egyenleg: {balance} $";
+                }
+                else
+                {
+                    BalanceText.Text = "Hiba: Nincs ilyen felhasználó.";
+                }
+            }
+            catch
+            {
+                BalanceText.Text = "Hiba az egyenleg lekérésekor.";
+            }
+        }
+
+        private void AddBalance_Click(object sender, RoutedEventArgs e)
+        {
+            ModifyBalance(isAddition: true);
+        }
+
+        private void WithdrawBalance_Click(object sender, RoutedEventArgs e)
+        {
+            ModifyBalance(isAddition: false);
+        }
+
+        private void ModifyBalance(bool isAddition)
+        {
+            if (!double.TryParse(AmountTextBox.Text.Trim(), out double amount) || amount <= 0)
+            {
+                MessageBox.Show("Adj meg érvényes összeget!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (!isAddition)
+                amount = -amount;
+
+            try
+            {
+                using var connection = new MySqlConnection(connectionString);
+                connection.Open();
+
+                var selectCmd = new MySqlCommand("SELECT egyenleg FROM felhasználók WHERE név = @nev", connection);
+                selectCmd.Parameters.AddWithValue("@nev", username);
+
+                double currentBalance = Convert.ToDouble(selectCmd.ExecuteScalar());
+                double newBalance = currentBalance + amount;
+
+                if (newBalance < 0)
+                {
+                    MessageBox.Show("Nincs elég egyenleg!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                var updateCmd = new MySqlCommand("UPDATE felhasználók SET egyenleg = @egyenleg WHERE név = @nev", connection);
+                updateCmd.Parameters.AddWithValue("@egyenleg", newBalance);
+                updateCmd.Parameters.AddWithValue("@nev", username);
+                updateCmd.ExecuteNonQuery();
+
+                MessageBox.Show($"Új egyenleg: {newBalance} $", "Siker", MessageBoxButton.OK, MessageBoxImage.Information);
+                AmountTextBox.Clear();
+                LoadBalance();
+            }
+            catch
+            {
+                MessageBox.Show("Adatbázis hiba!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
